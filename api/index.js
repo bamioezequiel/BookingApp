dfbbe7418e8ecdac04fac862b767385b3a3f06c6
ezequiel <ezequielbamiok@gmail.com>
2023-10-25 19:06:28 -0300

    first commit

--- api/index.js
+++ api/index.js
@@ -1,7 +1,32 @@
-import express from "express"
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 
+dotenv.config();
 const app = express();
 
-app.listen(8800, () => {
-    console.log("Connected to backend");
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_DB);
+    console.log("🟢 MongoDB connection succesfull");
+  } catch (error) {
+    throw error;
+  }
+};
+
+mongoose.connection.on("disconnected", () => {
+  console.log("MongoDB disconnected");
+});
+
+mongoose.connection.on("connected", () => {
+  console.log("MongoDB connected");
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello first request");
+});
+
+app.listen(process.env.PORT, () => {
+  connect();
+  console.log(`🟢 Server listening at ${process.env.PORT}`);
 });
